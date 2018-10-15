c67ef5dc74d3f23920bdbcaabc10db9914f07b08
Stefan J Bauer <35300659+stefnba@users.noreply.github.com>
2018-10-15 10:53:18 +0200

    Add files via upload
    New Version

--- VanWestendorp/main/models.py
+++ VanWestendorp/main/models.py
@@ -2,27 +2,39 @@ from django.db import models
 
 # Create your models here.
 
+def valid_pct(val):
+    if val.endswith("%"):
+       return float(val[:-1])/100
+    else:
+       try:
+          return float(val)
+       except ValueError:          
+          raise ValidationError(
+              _('%(value)s is not a valid pct'),
+                params={'value': value},
+           )
+
 class Answers(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     project = models.CharField(max_length=255, blank=True, null=True)
     name = models.CharField(max_length=255, blank=True, null=True)
-    exo_toocheap = models.DecimalField(decimal_places=2, max_digits=1000) 
-    exo_cheap = models.DecimalField(decimal_places=2, max_digits=1000) 
-    exo_epensive = models.DecimalField(decimal_places=2, max_digits=1000) 
-    exo_tooexpensive = models.DecimalField(decimal_places=2, max_digits=1000) 
-    basis_toocheap = models.DecimalField(decimal_places=2, max_digits=1000) 
-    basis_cheap = models.DecimalField(decimal_places=2, max_digits=1000) 
-    basis_epensive = models.DecimalField(decimal_places=2, max_digits=1000) 
-    basis_tooexpensive = models.DecimalField(decimal_places=2, max_digits=1000) 
-    plus_toocheap = models.DecimalField(decimal_places=2, max_digits=1000) 
-    plus_cheap = models.DecimalField(decimal_places=2, max_digits=1000) 
-    plus_epensive = models.DecimalField(decimal_places=2, max_digits=1000) 
-    plus_tooexpensive = models.DecimalField(decimal_places=2, max_digits=1000) 
-    dvv_toocheap = models.DecimalField(decimal_places=2, max_digits=1000) 
-    dvv_cheap = models.DecimalField(decimal_places=2, max_digits=1000) 
-    dvv_epensive = models.DecimalField(decimal_places=2, max_digits=1000) 
-    dvv_tooexpensive = models.DecimalField(decimal_places=2, max_digits=1000) 
+    exo_toocheap = models.CharField(max_length=255, validators=[valid_pct]) 
+    exo_cheap = models.CharField(max_length=255, validators=[valid_pct]) 
+    exo_epensive = models.CharField(max_length=255, validators=[valid_pct]) 
+    exo_tooexpensive = models.CharField(max_length=255, validators=[valid_pct]) 
+    basis_toocheap = models.CharField(max_length=255, validators=[valid_pct]) 
+    basis_cheap = models.CharField(max_length=255, validators=[valid_pct]) 
+    basis_epensive = models.CharField(max_length=255, validators=[valid_pct]) 
+    basis_tooexpensive = models.CharField(max_length=255, validators=[valid_pct]) 
+    plus_toocheap = models.CharField(max_length=255, validators=[valid_pct]) 
+    plus_cheap = models.CharField(max_length=255, validators=[valid_pct]) 
+    plus_epensive = models.CharField(max_length=255, validators=[valid_pct]) 
+    plus_tooexpensive = models.CharField(max_length=255, validators=[valid_pct]) 
+    dvv_toocheap = models.CharField(max_length=255, validators=[valid_pct]) 
+    dvv_cheap = models.CharField(max_length=255, validators=[valid_pct]) 
+    dvv_epensive = models.CharField(max_length=255, validators=[valid_pct]) 
+    dvv_tooexpensive = models.CharField(max_length=255, validators=[valid_pct]) 
 
 class Test(models.Model):
     name = models.CharField(max_length=255, blank=True, null=True)
-    dvv_epensive = models.DecimalField(decimal_places=2, max_digits=1000) 
+    dvv_epensive = models.CharField(max_length=255, validators=[valid_pct]) 
